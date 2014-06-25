31a095aee30587f40465d8ae3ca0969897059d30
deltafunction <frederik@orellana.dk>
2014-06-25 22:21:13 +0200

    Minor adjustments.

---
+++ apps/group_custom/templates/part.dialog.php
@@ -0,0 +1,13 @@
+<div id="new_group_dialog" title="<?php echo $l->t('New group'); ?>">
+    <table width="100%">
+        <tr>
+            <td><?php echo $l->t("Name") ; ?></td>
+            <td><input type="text" id="new_group" style="width: 90%;" /></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td style="padding: 0.5em; text-align: right;"><a class="button" href="#" id="add_group"><?php echo $l->t('Create')
+            ?></a></td>
+        </tr>
+    </table>
+<div>
