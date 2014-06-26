e8c79e157711aef6f03dd45143faaba6e53630b0
deltafunction <frederik@orellana.dk>
2014-06-26 13:52:15 +0200

    Small adjustments.

--- apps/firstrunwizard/templates/wizard.php
+++ apps/firstrunwizard/templates/wizard.php
@@ -26,7 +26,7 @@
 
 <?php if (OC_Util::getEditionString() === ''): ?>
 <h2><?php p($l->t('Upload and manage many and large files'));?></h2>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-webdav')); ?>">
+<a target="_blank" class="button" href="/themes/deic/support/#webdav">
 	<img class="appsmall svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/folder.svg')); ?>" /> <?php p($l->t('Access files via WebDAV'));?>
 </a>
 <?php else: ?>
@@ -41,7 +41,7 @@
 
 <p class="footnote">
 <?php if (OC_Util::getEditionString() === ''): ?>
-<?php print_unescaped($l->t('There’s more data.deic.dk specific information on our  <a target="_blank" href="https://data.deic.dk/themes/deic/support/">support pages</a>.', array(link_to_docs('user_manual')))); ?><br><?php print_unescaped($l->t('There’s more ownCloud information in the <a target="_blank" href="%s">documentation</a> and on the ownCloud <a target="_blank" href="http://owncloud.org">website</a>.', array(link_to_docs('user_manual')))); ?><br>
+<?php print_unescaped($l->t('There’s more data.deic.dk specific information on our  <a target="_blank" href="/themes/deic/support/">support pages</a>.', array(link_to_docs('user_manual')))); ?><br><?php print_unescaped($l->t('There’s more ownCloud information in the <a target="_blank" href="%s">documentation</a> and on the ownCloud <a target="_blank" href="http://owncloud.org">website</a>.', array(link_to_docs('user_manual')))); ?><br>
 <?php print_unescaped($l->t('If you like data.deic.dk, <a href="mailto:?subject=data.deic.dk&body=data.deic.dk is a great service for the Danish research community to store, share and sync files. All Danish researchers and students can log in at https://data.deic.dk/.">recommend us to your friends</a>!')); ?>
 <?php else: ?>
 © 2014 <a href="https://owncloud.com" target="_blank">ownCloud Inc.</a>
