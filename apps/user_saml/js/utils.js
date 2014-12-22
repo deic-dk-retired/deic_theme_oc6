c761822517e416cdef86d6568c33e608c4a98d47
cbri <cbri@dtu.dk>
2014-12-22 11:48:04 +0100

    Front page redesign

--- apps/user_saml/js/utils.js
+++ apps/user_saml/js/utils.js
@@ -99,13 +99,46 @@ $(document).ready(function(){
 
 
   /*$('<img class="kalmar2login" id="login-saml-img" src="https://data.deic.dk/themes/deic/core/img/kalmar2.png" title="'+ loginMsg +'" alt="'+ loginMsg +'" />').css( */
-  $('<img class="kalmar2login" id="login-saml-img" src="https://data.deic.dk/themes/deic/core/img/Login_Via_Institution_midLarge.png" title="'+ loginMsg +'" alt="'+ loginMsg +'" />').css(
+/*  $('<img class="kalmar2login" id="login-saml-img" src="https://data.deic.dk/themes/deic/core/img/Login_Via_Institution_midLarge.png" title="'+ loginMsg +'" alt="'+ loginMsg +'" />').css(
       {
         'padding': '0',
     'margin': '64px 0 20px 0',
     cursor : 'pointer',
     border : '0px solid #777'
       }).appendTo('#login-saml-action');
+*/
+  $('<div title="'+ loginMsg +'" alt="'+ loginMsg +'" class="kalmar2login" id="login-saml-img"><span style="color:#F7941E">Login</span> <span style="color:grey">via</span><br/> Institution</div>').css(
+      {
+        '-moz-box-shadow':' 2px 2px 2px 2px #ccc',
+        '-webkit-box-shadow':' 2px 2px 2px 2px #ccc',
+        'box-shadow':'1px 1px 1px 1px #ccc',
+        'background':'-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffffff), color-stop(1, #e3e3e3))',
+        'background':'-moz-linear-gradient(top, #ffffff 5%, #e3e3e3 100%)',
+        'background':'-webkit-linear-gradient(top, #ffffff 5%, #e3e3e3 100%)',
+        'background':'-o-linear-gradient(top, #ffffff 5%, #e3e3e3 100%)',
+        'background':'-ms-linear-gradient(top, #ffffff 5%, #e3e3e3 100%)',
+        'background':'linear-gradient(to bottom, #ffffff 5%, #e3e3e3 100%)',
+        'background-color':'#ffffff',
+        '-moz-border-radius':'5px',
+        '-webkit-border-radius':'5px',
+        'border-radius':'5px',
+        'border':'2px solid #83a000',
+        'display':'inline-block',
+        'cursor':'pointer',
+        'color':'#84a000',
+        'font-family': 'Open Sans',
+        'font-weight': '300',
+        'font-size':'20px',
+        'padding':'4px 24px',
+        'text-decoration':'none',
+        'text-shadow':'1px 1px 1px #ccc',
+        'text-align':'left',
+        'margin':'50px 20px',
+      }).appendTo('#login-saml-action');
+
+
+
+
 
   /*$('<br /><br /><br /><div><b> Or </b>').css( 
     {
@@ -127,13 +160,43 @@ $(document).ready(function(){
      'color': 'gray',
      }).appendTo('#login-saml'); */
 
-  $('<br /><img class="kalmar2login" id="login-guest-img" src="https://data.deic.dk/themes/deic/core/img/AlternativeLogin_midLarge.png" title="Manual login" alt="Manual login" /><br /><br /><br />').css(
-      {
-        'padding': '0',
-  'margin': '0 0 0 0',
-  cursor : 'pointer',
+//  $('<br /><img class="kalmar2login" id="login-guest-img" src="https://data.deic.dk/themes/deic/core/img/AlternativeLogin_midLarge.png" title="Manual login" alt="Manual login" /><br />').css(
+//      {
+//        'padding': '0',
+//  'margin': '0 0 0 0',
+//  cursor : 'pointer',
   //border : '1px solid #777'
+//      }).appendTo('#login-saml');
+
+  $('<div title="Manual login" alt="Manual login" class="kalmar2login" id="login-guest-img"><span style="color:#F7941E">Alternative</span><br/>Login</div>').css(
+      {
+        '-moz-box-shadow':' 2px 2px 2px 2px #ccc',
+        '-webkit-box-shadow':' 2px 2px 2px 2px #ccc',
+        'box-shadow':'1px 1px 1px 1px #ccc',
+        'background':'-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffffff), color-stop(1, #e3e3e3))',
+        'background':'-moz-linear-gradient(top, #ffffff 5%, #e3e3e3 100%)',
+        'background':'-webkit-linear-gradient(top, #ffffff 5%, #e3e3e3 100%)',
+        'background':'-o-linear-gradient(top, #ffffff 5%, #e3e3e3 100%)',
+        'background':'-ms-linear-gradient(top, #ffffff 5%, #e3e3e3 100%)',
+        'background':'linear-gradient(to bottom, #ffffff 5%, #e3e3e3 100%)',
+        'background-color':'#ffffff',
+        '-moz-border-radius':'5px',
+        '-webkit-border-radius':'5px',
+        'border-radius':'5px',
+        'border':'2px solid #83a000',
+        'display':'inline-block',
+        'cursor':'pointer',
+        'color':'#84a000',
+        'font-family': 'Open Sans',
+        'font-weight': '300',
+        'font-size':'20px',
+        'padding':'4px 20px',
+        'text-decoration':'none',
+        'text-shadow':'1px 1px 1px #ccc',
+        'text-align':'left',
+        'margin':'50px 20px',
       }).appendTo('#login-saml');
+
 
 
   /* $('<div> This is a test service, we can not guaranties the safety of your data at this point. This is an university serves and restricted to research data storage and exchange. It cannot be used for sensitive or illegale files./n For a description of the end user agreement, please click <a href="http://data.deic.dk/useragreement/">here</a>  </div>').css( 
@@ -156,7 +219,13 @@ $(document).ready(function(){
       }).appendTo('#login-saml');
 
 
-  $('fieldset').hide();
+  $('fieldset').css({'display':'inline-block'});
+  
+  if($('.warning').length || window.location.href.indexOf("lostpassword") > -1){
+  $('fieldset').show();
+  } else {
+    $('fieldset').hide();
+  }
 
   $('#login-guest-img').click(function(){
     if($('fieldset').is(":visible")){
@@ -164,8 +233,11 @@ $(document).ready(function(){
     }
     else{
       $('fieldset').show("slow", "linear");
-    }
+  }
   });
+
+
+
 
   var cookieName = "saml_auth_fail";
   var userCookieName = "saml_auth_fail_user";
--- core/templates/layout.guest.php
+++ core/templates/layout.guest.php
@@ -15,9 +15,14 @@
 <!--above change the title of the page in the browser tabs --> 
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="apple-itunes-app" content="app-id=543672169">
-		<link rel="shortcut icon" href="<?php print_unescaped(image_path('', 'favicon.png')); ?>" />
+
+        <meta property="og:title" content="DeIC data - Cloud storage for scientists" />
+        <meta property="og:description" content="DeIC data is a new service for the Danish research community to store, share and sync files." />
+        <meta property="og:image" content="https://data.deic.dk/themes/deic/core/img/logo_large.png" />
+
+        <link rel="shortcut icon" href="<?php print_unescaped(image_path('', 'favicon.png')); ?>" />
 		<link rel="apple-touch-icon-precomposed" href="<?php print_unescaped(image_path('', 'favicon-touch.png')); ?>" />
-		<?php foreach($_['cssfiles'] as $cssfile): ?>
+        <?php foreach($_['cssfiles'] as $cssfile): ?>
 			<link rel="stylesheet" href="<?php print_unescaped($cssfile); ?>" type="text/css" media="screen" />
 		<?php endforeach; ?>
 		<?php foreach($_['jsfiles'] as $jsfile): ?>
@@ -33,25 +38,162 @@
 				print_unescaped('/>');
 			?>
 		<?php endforeach; ?>
-	</head>
+    
+        <script src="/3rdparty/bpopup/jquery.bpopup.min.js"></script>
+</head>
 
-	<body id="body-login">
-		<div id="login">
-			<header><div id="header">
-				<a href="/"><img src="<?php print_unescaped(image_path('', 'logo-Deic-data.png')); ?>" class="svg" alt="ownCloud" /></a> <!--&nbsp; <font size=18><b>data</b></font></a> -->
-				<div id="logo-claim" style="display:none;"><?php p($defaults->getLogoClaim()); ?></div>
-			</div></header>
-			<?php print_unescaped($_['content']); ?>
-		</div>
-		<footer>
-                        <center>
-			<p class="string" style="color: grey; text-align: left; width: 600px;">
-			This is a test service. We do not guarantee the safety of your data and do not accept any liability for the consequences of the use of the service. Uploading illegal or copyrighted data for which you cannot claim ownership is strictly forbidden. For full terms and conditions, please click <a href="http://data.deic.dk/themes/deic/terms/"><b>here</b></a>  <!--need to create this link! --> 
-			</p>
-<br />
-<br />
-			<p class="info">
-                                DeIC data is powered by <a href="http://owncloud.org/">ownCloud</a>
-			</p></footer>
-	</body>
+    <body id="body-login">
+      
+
+      <div id="bodywrapper">
+        <div id="maincontent">
+          <div id="beta_tag"><img src="<?php print_unescaped(image_path('', 'beta.png')); ?>" width="100px"></div>
+          <div id="login">
+            <div id="header">
+              <a href="/"><img src="<?php print_unescaped(image_path('', 'logo-trnsp.png')); ?>" class="svg" alt="ownCloud" /><span id="logodata" class="deic_green">data</span></a> <!--&nbsp; <font size=18><b>data</b></font></a> -->
+              <div id="logo-claim" style="display:none;"><?php p($defaults->getLogoClaim()); ?></div>
+            </div>
+            <?php print_unescaped($_['content']); ?>
+          </div>
+          <div id="tagline">Cloud storage for <span class="deic_green">scientists</span></div>
+        </div>
+      </div>
+      <div id="footer">
+        <div id="hrule"></div> 
+        <div id="info_buttons"> 
+          <a id="about">about this service</a>
+          <div id="aboutPop" class="popup">
+            <div class="pad">
+               <span class="b-close">
+               <img class="svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'actions/close.svg')); ?>">
+               </span>
+               <h2>About this service</h2>
+               <p> data.deic.dk is an online data storage and synchronization service provided by the Danish 
+                   e-Infrastructure Cooperation (<a class="deic_green" href="http://www.deic.dk">DeIC</a>) specifically aimed at researchers and scientists
+                   at Danish academic institutions. 
+               </p><p>
+                   Built on open-source technology, data.deic.dk has been made for safe-keeping research data which potentially consist
+                   of very large files. data.deic.dk therefore offers separate storage areas for files that
+                   need to be sync'ed to desktops, laptops, and mobile devices (i.e., documents, notes, slides, 
+                   teaching material, etc.) and for files that are not suitable for sync'ing (i.e., large data sets). 
+                   We also offer chunked uploading, so that upload of large files can be resumed if the 
+                   connection is temporarily lost. In addition, data.deic.dk supports import of data from external
+                   providers through http or ftp.
+               </p><p>
+                   data.deic.dk is attached directly to "Forskningsnettet" so that clients accessing our
+                   servers from within any Danish academic institution will experience up- and download speeds of
+                   up to 10 Giga-bits/second, which is similar to accessing an external USB hard drive.  
+               </p><p>
+                   
+               </p>
+             </div>
+          </div>
+            <script>;(function($) {
+                $(function() {
+                  $('a#about').bind('click', function(e) {
+                    e.preventDefault();
+                    $('#aboutPop').bPopup( {opacity: 0.4,modalColor: '#000'} );
+                  });
+                });
+                })(jQuery); 
+           </script>
+
+          <a id="terms">terms and conditions</a>
+          <div id="termsPop" class="popup">
+            <div class="pad">
+               <span class="b-close">
+                  <img class="svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'actions/close.svg')); ?>">
+               </span>
+               <h2>Terms and conditions</h2>
+               <p> 
+                   In order to use data.deic.dk, you must comply to these terms and all applicable laws.
+                   By using any service provided by data.deic.dk you implicitly agree to be bound by these terms.               
+               </p>
+               <P>  
+                  (i) In order to use data.deic.dk you have to be affiliated to an 
+                    academic institution located within Denmark. We may, in sole discretion, grant or revoke 
+                    exceptions to this rule on an individual basis. 
+               </p>
+                <p>
+                    (ii) data.deic.dk is provided "as is". We do not give any warranty and we can not guarantee 
+                    the safety of your data, neither do we accept any liability for loss or damage of data.
+                    We can also not be held responsible for unintentional deletion or modification of files.
+                    We do not guarantee the availability of our service and we are not liable for neither planned
+                    nor unexpected "down time".  
+                     
+               </p> 
+               <p>    
+                  (iii) Uploading illegal or copyrighted material for which you cannot claim ownership is strictly 
+                    forbidden. It is your responsibility to ensure that content stored within your account on
+                    data.deic.dk does not violate these terms. Passwords for accessing the service are strictly 
+                    personal and it is not allowed to share passwords with any third party.
+               </p>
+               <p>
+                  (iv) We reserve the right to revise these terms at any time, with or without prior notice.
+               </p>
+               <p>
+                  (v) We will, from time to time, deploy necessary changes, updates, or enhancements to 
+                    data.deic.dk. We may add, suspend, or remove features or functionalities.
+               </p>
+                <p>
+                  (vi) We reserve the right to terminate or suspend your account on data.deic.dk, with or 
+                    without prior notice. You acknowledge that in this case, you may no longer have access to
+                    any material stored with data.deic.dk.
+               </p>
+             </div>
+          </div>
+            <script>;(function($) {
+                $(function() {
+                  $('a#terms').bind('click', function(e) {
+                    e.preventDefault();
+                    $('#termsPop').bPopup( {opacity: 0.4, modalColor: '#000'} );
+                  });
+                });
+                })(jQuery); 
+           </script>
+                  <a id="next">what's next?</a>
+                  <div id="nextPop" class="popup">
+<div class="pad">
+               <span class="b-close">
+                 <img class="svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'actions/close.svg')); ?>">
+               </span>
+               <h2>What's next?</h2>
+               <p>
+                  - compute.deic.dk 
+               </p> 
+               <p>
+                  - Integrated file meta data 
+               </p> 
+               <p>
+                  - Real-time collaborative editing (Open Office, Latex, ...)  
+               </p> 
+               <p>
+                  - iPython interface 
+               </p> 
+               <p>
+                  - deic.data.dk API 
+               </p> 
+</div>
+          </div>
+            <script>;(function($) {
+                $(function() {
+                  $('a#next').bind('click', function(e) {
+                    e.preventDefault();
+                    $('#nextPop').bPopup( {opacity: 0.4, modalColor: '#000'} );
+                  });
+                });
+                })(jQuery); 
+           </script>
+
+</div>
+                
+
+
+
+
+<p class="footinfo">                  
+DeIC data is powered by <a href="http://owncloud.org/">ownCloud</a>                  
+</p>                 
+</div>           
+</body>                  
 </html>
