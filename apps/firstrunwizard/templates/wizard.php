efd208c50307de707b308095bda6726dd43a91dd
Lars Næsbye Christensen <lars@naesbye.dk>
2016-06-08 10:28:00 +0200

    Revert "small fixes"
    This reverts commit 265484e7afeff496115233f4917743d4fd521d97.

--- apps/firstrunwizard/templates/wizard.php
+++ apps/firstrunwizard/templates/wizard.php
@@ -1,56 +1,53 @@
 <div id="firstrunwizard">
+    <div class="wrap-wizard">
+        <a id="closeWizard" class="close">
+            <img class="svg" src="/core/img/actions/close.svg">
+        </a>
 
-<a id="closeWizard" class="close">
-	<img class="svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'actions/close.svg')); ?>">
-</a>
-
-<h1><?php p($l->t('Welcome to'));?> <?php p($theme->getTitle()); ?></h1>
-<?php if (OC_Util::getEditionString() === ''): ?>
-<p><?php p($l->t('Your personal web services. All your files, contacts, calendar and more, in one place.'));?></p>
-<?php else: ?>
-<p><?php p($theme->getSlogan()); ?></p>
-<?php endif; ?>
-
-
-<h2><?php p($l->t('Get the apps to sync your files'));?></h2>
-<a target="_blank" href="<?php p($_['clients']['desktop']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'desktopapp.png')); ?>" />
-</a>
-<a target="_blank" href="<?php p($_['clients']['android']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'googleplay.png')); ?>" />
-</a>
-<a target="_blank" href="<?php p($_['clients']['ios']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'appstore.png')); ?>" />
-</a>
-
-<?php if (OC_Util::getEditionString() === ''): ?>
-<h2><?php p($l->t('Connect your desktop apps to'));?> <?php p($theme->getName()); ?></h2>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-calendars')) ?>">
-	<img class="appsmall appsmall-calendar svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/calendar-dark.svg')); ?>" /> <?php p($l->t('Connect your Calendar'));?>
-</a>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-contacts')) ?>">
-	<img class="appsmall appsmall-contacts svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/contacts-dark.svg')); ?>" /> <?php p($l->t('Connect your Contacts'));?>
-</a>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-webdav')); ?>">
-	<img class="appsmall svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/folder.svg')); ?>" /> <?php p($l->t('Access files via WebDAV'));?>
-</a>
-<?php else: ?>
-<br><br><br>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-manual')); ?>">
-	<img class="appsmall svg" src="<?php print_unescaped(OCP\Util::imagePath('settings', 'help.svg')); ?>" /> <?php p($l->t('Documentation'));?>
-</a>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-webdav')); ?>">
-	<img class="appsmall svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/folder.svg')); ?>" /> <?php p($l->t('Access files via WebDAV'));?>
-</a>
-<?php endif; ?>
-
-<p class="footnote">
-<?php if (OC_Util::getEditionString() === ''): ?>
-<?php print_unescaped($l->t('There’s more information in the <a target="_blank" href="%s">documentation</a> and on our <a target="_blank" href="http://owncloud.org">website</a>.', array(link_to_docs('user_manual')))); ?><br>
-<?php print_unescaped($l->t('If you like ownCloud, <a href="mailto:?subject=ownCloud&body=ownCloud is a great open software to sync and share your files. You can freely get it from http://owncloud.org">recommend it to your friends</a>!')); ?>
-<?php else: ?>
-© 2014 <a href="https://owncloud.com" target="_blank">ownCloud Inc.</a>
-<?php endif; ?>
-</p>
-
+        <h2>Welcome to DeIC data - <small>powered by ownCloud</small></h2>
+        <p style="text-align: center;"><em>All your research data and files in one place</em></p>
+        <br>
+        <br>
+        <div class="wizard-steps cf">
+            <div class="wizard-step-count">1</div>
+            <div class="wizard-step-content">
+                <h3>First, <a href="/index.php/settings/personal">set a password</a>, to allow access from your phone, tablet and desktop</h3>
+            </div>
+            <div class="wizard-step-count">2</div>
+            <div class="wizard-step-content">
+                <h3>Download synchronization apps</h3>
+                <a target="_blank" href=" http://owncloud.org/sync-clients/">
+                    <img src="/core/img/desktopapp.png">
+                </a>
+                <a target="_blank" href="https://play.google.com/store/apps/details?id=com.owncloud.android">
+                    <img src="/core/img/googleplay.png">
+                </a>
+                <a target="_blank" href="https://itunes.apple.com/us/app/owncloud/id543672169?mt=8">
+                    <img src="/core/img/appstore.png">
+                </a>
+            </div>
+            <div class="wizard-step-count">3</div>
+            <div class="wizard-step-content">
+                <h3>Upload and manage many and large files</h3>
+                <a target="_blank" class="button" href="/themes/deic/support/#webdav">
+                    <img class="appsmall svg" src="/themes/deic/core/img/places/folder.svg"> Access files through WebDAV</a>
+            </div>
+            <div class="wizard-step-count">4</div>
+            <div class="wizard-step-content">
+                <h3>Recommend us</h3>
+                <p>
+                    If you like data.deic.dk, recommend us to your friends!<br>
+                    <a href="mailto:?subject=data.deic.dk&amp;body=data.deic.dk is a great service for the Danish research community to store, share and sync files. All Danish researchers and students can log in at https://data.deic.dk/." class="button">Email</a>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=https://data.deic.dk/" target="_blank" class="button">Facebook</a>
+                    <a href="http://twitter.com/home?status=DeIC%20data%20is%20a%20great%20service%20for%20the%20Danish%20research%20community%20to%20store%2C%20share%20and%20sync%20files.%20http%3A%2F%2Fdata.deic.dk" target="_blank" class="button">Twitter</a>
+                    <a href="http://www.linkedin.com/shareArticle?mini=true&url=http://data.deic.dk/&title=DeIC%20data%20is%20a%20great%20service%20for%20the%20Danish%20research%20community%20to%20store%2C%20share%20and%20sync%20files.%20All%20Danish%20researchers%20and%20students%20can%20log%20in" target="_blank" class="button">LinkedIn</a>
+                </p>
+            </div>
+        </div>
+        <p class="footnote">
+            <small>
+                There’s more data.deic.dk specific information on our  <a target="_blank" href="/themes/deic/support/">support pages</a>.<br>There’s more ownCloud information in the <a target="_blank" href="http://doc.owncloud.org/server/6.0/go.php?to=user_manual">documentation</a> and on the ownCloud <a target="_blank" href="http://owncloud.org">website</a>.
+            </small>
+        </p>
+    </div>
 </div>
